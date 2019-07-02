b41b10e8368500604eb05b57f565159879ab21fe
cuilihua1984@163.com <cuilihua1984@163.com>
2019-07-02 17:19:56 +0800

    commit UserAction.java

--- src/main/java/com/coding/sales/cx/user/User.java
+++ src/main/java/com/coding/sales/cx/user/User.java
@@ -1,5 +1,12 @@
 package com.coding.sales.cx.user;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class User {
+    public String userName ;
+    public String cardNo;
+    public String cardType;
+    public String integrarl;
 
 }
